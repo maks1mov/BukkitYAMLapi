e9b8dc3422b66c2054f0571163fc769f32461724
romas <fuckoff@maks1mov.ru>
2025-04-23 11:55:18 +0700

    gffgf

--- BukkitYAMLapi/src/main/java/bukkitYaml/YAML.java
+++ BukkitYAMLapi/src/main/java/bukkitYaml/YAML.java
@@ -8,36 +8,52 @@ import bukkitYaml.file.YamlConfiguration;
 
 public class YAML {
 
-	private final File file;
-	private final FileConfiguration YAML;
-	
-	public YAML(File file) throws IOException, InvalidConfigurationException {
-		
-	    this.file = file;
-	    
-	    YAML = new YamlConfiguration();
-	    	
-	    if(!file.exists())
-	        file.createNewFile();
-	    
-	    else YAML.load(file);
+	private File file;
+	private FileConfiguration YAML;
+
+	public YAML(File file) {
+
+		try {
+			this.file = file;
+
+			YAML = new YamlConfiguration();
+
+			if (!file.exists())
+				file.createNewFile();
+
+			else
+				YAML.load(file);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
+
 	public FileConfiguration getYAML() {
 		return YAML;
 	}
-	
-	public void save() throws IOException {
-		YAML.save(file);
-	}
-	
-	public void reload() throws IOException, InvalidConfigurationException {
-		
-		if(file.exists()) {
+
+	public void save() {
+		try {
 			YAML.save(file);
-			YAML.load(file);	
-		} 
-		
-		else YAML.save(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-} 
+
+	public void reload() {
+
+		try {
+			if (file.exists()) {
+				YAML.save(file);
+				YAML.load(file);
+			}
+
+			else
+				YAML.save(file);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
